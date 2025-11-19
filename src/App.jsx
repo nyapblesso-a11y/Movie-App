cca1eaa14eb64e4b514e1e391b388fb7d40b0459
nyapblesso-a11y <nyapblesso@gmail.com>
2025-11-19 14:33:17 +0100

    the header was set up in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div></div>
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+         <h1>Find <span className='text-gradient'>Movies</span> you'll enjoy</h1>
+        </header>
+      </div>
+    </main>
   )
 }
 
