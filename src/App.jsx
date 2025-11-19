8b98e0fd0c915402b2bbc9bf45ad295da27acc79
nyapblesso-a11y <nyapblesso@gmail.com>
2025-11-19 15:04:48 +0100

    the vite api key was obtain from the tmdb site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './component/Search'
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:`Bearer ${API_KEY}`
+  }
+}
 
  const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
 
+  
+  useEffect(() => {
+
+  }, [])
   return (
     <main>
       <div className='pattern'/>
